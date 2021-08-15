d244020696e0359ffecda51e80fa5feb064ff653
郑鑫昌 <zhengxinchang>
2021-08-15 10:32:06 +0800

    力扣刷题第二天

--- Leetcode/src/DayExercise/Day02.java
+++ Leetcode/src/DayExercise/Day02.java
@@ -3,18 +3,25 @@ package DayExercise;
 /**
  * @author ZhengXinchang
  * @create 2021-08-13-8:59
- * 给你一份 n 位朋友的亲近程度列表，其中 n 总是 偶数 。
- * 对每位朋友 i，preferences[i] 包含一份 按亲近程度从高到低排列 的朋友列表。换句话说，排在列表前面的朋友与 i 的亲近程度比排在列表后面的朋友更高。每个列表中的朋友均以 0 到 n-1 之间的整数表示。
- * 所有的朋友被分成几对，配对情况以列表 pairs 给出，其中 pairs[i] = [xi, yi] 表示 xi 与 yi 配对，且 yi 与 xi 配对。
- * 但是，这样的配对情况可能会是其中部分朋友感到不开心。在 x 与 y 配对且 u 与 v 配对的情况下，如果同时满足下述两个条件，x 就会不开心：
- * x 与 u 的亲近程度胜过 x 与 y，且
- * u 与 x 的亲近程度胜过 u 与 v
  */
 public class Day02 {
+    /**
+     * * 给你一份 n 位朋友的亲近程度列表，其中 n 总是 偶数 。
+     * * 对每位朋友 i，preferences[i] 包含一份 按亲近程度从高到低排列 的朋友列表。换句话说，排在列表前面的朋友与 i 的亲近程度比排在列表后面的朋友更高。每个列表中的朋友均以 0 到 n-1 之间的整数表示。
+     * * 所有的朋友被分成几对，配对情况以列表 pairs 给出，其中 pairs[i] = [xi, yi] 表示 xi 与 yi 配对，且 yi 与 xi 配对。
+     * * 但是，这样的配对情况可能会是其中部分朋友感到不开心。在 x 与 y 配对且 u 与 v 配对的情况下，如果同时满足下述两个条件，x 就会不开心：
+     * * x 与 u 的亲近程度胜过 x 与 y，且
+     * * u 与 x 的亲近程度胜过 u 与 v
+     *
+     * @param n
+     * @param preferences
+     * @param pairs
+     * @return
+     */
     public static int unhappyFriends(int n, int[][] preferences, int[][] pairs) {
         int[][] order = new int[n][n];
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n-1; j++) {
+            for (int j = 0; j < n - 1; j++) {
                 order[i][preferences[i][j]] = j;
             }
         }
@@ -41,16 +48,35 @@ public class Day02 {
         return unhappyCount;
     }
 
+    /**
+     * 最长递增子序列
+     * 给你一个整数数组 nums ，找到其中最长严格递增子序列的长度。
+     * 子序列是由数组派生而来的序列，删除（或不删除）数组中的元素而不改变其余元素的顺序。例如，[3,6,2,7] 是数组 [0,3,1,6,2,2,7] 的子序列。
+     *
+     * @param nums
+     * @return
+     */
+    public static int lengthOfLIS(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int[] dp = new int[nums.length];
+        dp[0] = 1;
+        int maxans = 1;
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            maxans = Math.max(maxans, dp[i]);
+        }
+        return maxans;
+    }
+
 
     public static void main(String[] args) {
-        int n = 4;
-        int[][] preferences = new int[][]{
-                {1, 2, 3},
-                {3, 2, 0},
-                {3, 1, 0},
-                {1, 2, 0}};
-        int[][] pairs = new int[][]{{0, 1}, {2, 3}};
-        unhappyFriends(n, preferences, pairs);
-        System.out.println();
+        int[] nums=new int[]{1,3,5,4,7};
     }
 }
